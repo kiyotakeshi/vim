01a4e975ceef2c16565eac2881298909170b5062
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2019-11-15 17:12:47 +0900

    daw, <C-r>, >G, *, cw, f

--- code_edit/the_vim_way/3_concat.js
+++ code_edit/the_vim_way/3_concat.js
@@ -1,9 +1,10 @@
-/***
- * Excerpted from "Practical Vim",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/dnvim for more book information.
-***/
 var foo = "method("+argument1+","+argument2+")";
+
+// f+         -> マッチした文字までカーソルを移動
+// s + <Esc>  ->  カーソルの文字を削除して挿入モードに入る
+// ;          ->  直前の f の検索を繰り返す(戻す場合は , )
+// .
+// ;.
+// ;.
+
+var foo = "method(" + argument1 + "," + argument2 + ")";
