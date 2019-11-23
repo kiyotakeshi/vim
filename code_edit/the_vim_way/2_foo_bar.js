004efe22cc5db678c01ac96b6d464a94a50efabf
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2019-11-23 12:50:49 +0900

    :,R,<C-v>,gv,>,c

--- code_edit/the_vim_way/2_foo_bar.js
+++ code_edit/the_vim_way/2_foo_bar.js
@@ -6,6 +6,22 @@ var foobar = foo + bar
 // j
 // .
 // j.
+
+var foo = 1;
+var bar = 'a';
+var foobar = foo + bar;
+
+
+// -----------------------
+
+var foo = 1
+var bar = 'a'
+var foobar = foo + bar
+
+// <C-v>jj$
+// A;
+// <Esc>    -> ctl+c で抜けるとうまく反映されない
+
 var foo = 1;
 var bar = 'a';
 var foobar = foo + bar;
--- code_edit/visual_mode/fibonacci-malformed.py
+++ code_edit/visual_mode/fibonacci-malformed.py
@@ -4,3 +4,14 @@ def fib(n):
 print a,
 a, b = b, a+b
 fib(42)
+
+// print a の行から
+// Vj
+// >.
+
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+      print a,
+      a, b = b, a+b
+fib(42)
